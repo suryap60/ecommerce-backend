9052d45584d8863b4f8d0dada4f247ab3726e8b0
suryap60 <suryap6061@gmail.com>
2026-03-12 08:26:48 +0530

    fix: server issues

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ mongoose
   .catch((err) => console.log(err));
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
